ab97cdf0668fc88d0c5b82480f4a3d801f0fa1a6
Nubuki-all <121189493+Nubuki-all@users.noreply.github.com>
2025-04-26 01:11:05 +0100

    update client.py

--- neonize/aioze/client.py
+++ neonize/aioze/client.py
@@ -648,7 +648,8 @@ class NewAClient:
         field_name = (
             partial_message.__class__.__name__[0].lower() + partial_message.__class__.__name__[1:]
         )  # type: ignore
-        quoted.contextInfo.Clear()
+        if quoted.HasField("contextInfo"):
+            quoted.contextInfo.Clear()
         partial_message.contextInfo.MergeFrom(self._make_quoted_message(quoted, reply_privately))
         getattr(build_message, field_name).MergeFrom(partial_message)
         return build_message
