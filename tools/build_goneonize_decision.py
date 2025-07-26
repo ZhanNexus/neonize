2f2c66d9c479235059f1b2a06ccb0adf3d4e06f1
Nubuki-all <121189493+Nubuki-all@users.noreply.github.com>
2025-07-26 12:03:27 +0100

    update

--- tools/build_goneonize_decision.py
+++ tools/build_goneonize_decision.py
@@ -59,12 +59,8 @@ def build_goneonize_decision() -> bool:
         bool: True if Goneonize should be rebuilt, False otherwise.
     """
     try:
-        return True
         github = Github()
-        zipfile = ZipFile(
-            BytesIO(
-                github.download_neonize(
-                    github.get_last_goneonize_version())))
+        zipfile = ZipFile(BytesIO(github.download_neonize(github.get_last_goneonize_version())))
         return check(zipfile)
     except Exception:
         return True
