e028e1859c6a557c8e0c44bb521ed283aad0b89f
Nubuki-all <121189493+Nubuki-all@users.noreply.github.com>
2025-07-26 12:07:00 +0100

    update

--- neonize/download.py
+++ neonize/download.py
@@ -1,13 +1,11 @@
 import os
-from pathlib import Path
-
+from .utils.platform import generated_name
 import requests
+from pathlib import Path
 from tqdm import tqdm
 
-from .utils.platform import generated_name
-
-__GONEONIZE_VERSION__ = "0.3.9.1"
-__GIT_RELEASE_URL__ = "https://github.com/nubuki-all/neonize"
+__GONEONIZE_VERSION__ = "0.3.10.4"
+__GIT_RELEASE_URL__ = "https://github.com/krypton-byte/neonize"
 
 
 class UnsupportedPlatform(Exception):
@@ -23,11 +21,7 @@ def __download(url: str, fname: str, chunk_size=1024):
     with (
         open(fname, "wb") as file,
         tqdm(
-            desc=Path(fname).name,
-            total=total,
-            unit="iB",
-            unit_scale=True,
-            unit_divisor=1024,
+            desc=Path(fname).name, total=total, unit="iB", unit_scale=True, unit_divisor=1024
         ) as bar,
     ):
         for data in resp.iter_content(chunk_size=chunk_size):
