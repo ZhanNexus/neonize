94804162c3741ca5202e28c3a7ed0fc0fefd6caf
ZulX88 <zhanprasetyaningrat@gmail.com>
2025-09-19 23:33:53 +0700

    Update little

--- neonize/download.py
+++ neonize/download.py
@@ -6,8 +6,8 @@ from tqdm import tqdm
 
 from .utils.platform import generated_name
 
-__GONEONIZE_VERSION__ = "0.3.10.4"
-__GIT_RELEASE_URL__ = "https://github.com/Nubuki-all/neonize"
+__GONEONIZE_VERSION__ = "v1"
+__GIT_RELEASE_URL__ = "https://github.com/ZulX88/neonize"
 
 
 class UnsupportedPlatform(Exception):
