25191a3e88435011d8858d1487edd8a9fb8dc007
zhann <zhanprasetyaningrat@gmail.com>
2026-01-18 16:16:47 +0700

    Y

--- neonize/download.py
+++ neonize/download.py
@@ -3,8 +3,8 @@ from pathlib import Path
 import requests
 from tqdm import tqdm
 from .utils.platform import generated_name
-__GONEONIZE_VERSION__ = '0.3.13.6'
-__GIT_RELEASE_URL__ = 'https://github.com/ZulX88/neonize'
+__GONEONIZE_VERSION__ = '0.3.14.post0'
+__GIT_RELEASE_URL__ = 'https://github.com/ZhanNexus/neonize'
 
 class UnsupportedPlatform(Exception):
     pass
